1d21363cbeb45c3485a63fcce088f615442decd3
Sarmad-coder <sarmadawan35@gmail.com>
2023-08-20 23:33:17 +0500

    Initial commit

---
+++ src/services/config.js
@@ -0,0 +1,4 @@
+// const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://api.ruuby.co"
+// const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://95.179.207.65:9000"
+const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
+export { medusaUrl }
